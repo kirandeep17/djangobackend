e56c63816afc40690a9a75fd883429e23bf878ed
kirandeep17 <gagankumar2050@gmail.com>
2023-08-11 11:53:56 -0400

    j

--- auth/settings.py
+++ auth/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 import cloudinary
 import cloudinary.uploader
@@ -92,11 +93,9 @@ WSGI_APPLICATION = 'auth.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'howitworksdev',
-        'USER': 'root',
-        'PASSWORD': 'root'
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'howitworks'
     }
 }
 
